e60040a6765529b03e413e00115c70904bed6c26
Jefferson Neves <jefferson.rpn@gmail.com>
2018-04-26 21:06:36 -0300

    Gotta catch 'em all

---
+++ README.md
@@ -0,0 +1,7 @@
+# Pokégear bot
+
+Um bot para Discord para informar spaws e organizar raids no Pokémon Go.
+
+## Adicionar o bot ao seu servidor
+
+https://discordapp.com/oauth2/authorize?&client_id=439210693938511882&scope=bot&permissions=0
---
+++ bot.js
@@ -0,0 +1,38 @@
+var Discord = require('discord.io');
+var logger = require('winston');
+var auth = require('./auth.json');
+// Configurar log
+logger.remove(logger.transports.Console);
+logger.add(logger.transports.Console, {
+    colorize: true
+});
+logger.level = 'debug';
+// Inicializar bot Discord
+var bot = new Discord.Client({
+   token: auth.token,
+   autorun: true
+});
+bot.on('ready', function (evt) {
+    logger.info('Connected');
+    logger.info('Logged in as: ');
+    logger.info(bot.username + ' - (' + bot.id + ')');
+});
+bot.on('message', function (user, userID, channelID, message, evt) {
+    // Escuta mensagens que comecem com `!`
+    if (message.substring(0, 1) == '!') {
+        var args = message.substring(1).split(' ');
+        var cmd = args[0];
+
+        args = args.splice(1);
+        switch(cmd) {
+            // !ping
+            case 'ping':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Pong!'
+                });
+            break;
+            // Just add any case commands if you want to..
+         }
+     }
+});
